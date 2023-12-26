74ffd8243026385cf1d0d05fe209a11a4713a54f
Scot Watson <97048622+ScotWatson@users.noreply.github.com>
2023-12-26 15:06:43 -0500

    Create index.js

---
+++ docs/index.js
@@ -0,0 +1,48 @@
+/*
+(c) 2023 Scot Watson  All Rights Reserved
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
+const initPageTime = performance.now();
+
+const asyncWindow = new Promise(function (resolve, reject) {
+  window.addEventListener("load", function (evt) {
+    resolve(evt);
+  });
+});
+
+(async function () {
+  try {
+    const modules = await Promise.all( [ asyncWindow ] );
+    start(modules);
+  } catch (e) {
+    console.error(e);
+    throw e;
+  }
+})();
+
+async function start( [ evtWindow ] ) {
+  try {
+    (async function () {
+      const request = new Request("https://api.w3.org/translations/", {
+        method: "GET",
+        headers: [],
+        mode: "cors",
+        credentials: "same-origin",
+        cache: "",
+        redirect: "follow",
+        referrer: "about:client",
+        referrerPolicy: "",
+        integrity: "",
+        keepalive: "",
+        signal: null,
+        priority: "auto",
+      });
+      const response = await fetch(request);
+      const text = document.createTextNode(await response.body.text());
+      document.body.appendChild(text);
+    })();
+  } catch (e) {
+    console.log(e);
+  }
+}
